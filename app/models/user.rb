554af3d61ff996e13c9543e300ad5689d75352b9
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-12 18:35:32 +0900

    モデルファイルへのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name, presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana, presence: true,
+    format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, presence: true,
+    format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
+
 end
