2007fe735ef250a5fb779185e7eae0d7a38b38a4
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-13 23:41:30 +0900

    コントローラーでnewとcreateの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(create_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user_id, :item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :necessary_time_id, :price)
   end
 end
