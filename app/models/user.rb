6381e288c71d80ed70ef33a75d6cccdcc8660b7b
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-13 17:54:35 +0900

    修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :first_name, presence: true,
-    format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name, presence: true,
-    format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name_kana, presence: true,
     format: { with: /\A[ァ-ヶー－]+\z/}
   validates :last_name_kana, presence: true,
