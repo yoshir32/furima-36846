ba5d920da447d360d4f2e68ecd7dbfc883e9e419
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-14 00:21:05 +0900

    imageのitemsへの紐付けとストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:user_id, :item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :necessary_time_id, :price)
+    params.require(:item).permit(:user_id, :item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :necessary_time_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
 
   validates :item_name, presence: true
   validates :item_description, presence: true
