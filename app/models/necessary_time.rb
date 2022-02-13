19583919bdf1f6e28d2175d9052d3e28e4d4f7c5
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-14 01:37:47 +0900

    ActiveHshのモデルにデータを定義

--- app/models/necessary_time.rb
+++ app/models/necessary_time.rb
@@ -1,5 +1,11 @@
 class Necessary_time < ActiveHash::Base
 
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '2~3日で発送' },
+    { id: 4, name: '3~7日で発送' }
+  ]
 
   include ActiveHash::Associations
   has_many :items
