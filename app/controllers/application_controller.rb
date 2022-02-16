83c4135be7438b451191e8cdf902384792b32f1e
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-17 00:01:31 +0900

    authenticate_userの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private 
