bef3d78dc61a9fab0c4e5630f18332ef0782c087
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-16 23:50:46 +0900

    priceのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,8 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :necessary_time_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } 
+    format: { with: /\A[0-9]+\z/}
 
 
 end
