b370c63c571074ebeb860e9fd9df2906b7423152
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-17 18:02:21 +0900

    ストロングパラメーターの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create!(item_params)
+    redirect_to root_path
   end
 
   private
   def item_params
-    params.require(:item).permit(:user_id, :item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :necessary_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :necessary_time_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
