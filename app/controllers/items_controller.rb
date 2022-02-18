de579dd4e547572d6a6c48bf5b0edbf7bb4850fa
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-18 11:14:27 +0900

    バリデーションによる遷移先の分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
-    redirect_to root_path
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
