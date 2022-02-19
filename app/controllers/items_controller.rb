dc104841d118bad2d6ab49d491a402b1742d16b0
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-19 16:52:58 +0900

    表示順を新しいものが上に来るよう変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: %i[show index]
   
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
