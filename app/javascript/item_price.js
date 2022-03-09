1f49f20fbaa109dcde08eed97164292bed892209
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-03-09 21:12:14 +0900

    販売利益の表示を修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -7,13 +7,14 @@ priceInput.addEventListener("input", () => {
    addTaxDom.innerHTML = Math.floor(inputValue*0.1)
 })
 })
-
 window.addEventListener('load', function(){
 
   const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
    const inputValue = priceInput.value;
-   const addTaxDom = document.getElementById("profit");
-   addTaxDom.innerHTML = Math.floor(inputValue*0.9)
+   const valueResult = Math.floor(inputValue*0.1);
+   const profitDom = document.getElementById("profit");
+   profitDom.innerHTML = Math.floor(inputValue - valueResult)
 })
 })
+
